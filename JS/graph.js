1c0e65b660b28da8f333c9ad4356ab48df390572
Dimuth <dimuthu.13@cse.mrt.ac.lk>
2016-10-25 16:56:32 +0530

    re organized the code, color schema introduced to seperate data points based on time. But it is not still working

---
+++ JS/graph.js
@@ -0,0 +1,54 @@
+/**
+ * Created by dimuth on 10/25/16.
+ */
+function setCalendarDate(start){
+    var calendarTime=timeStampToCalendarDate(start);
+    document.getElementById("calenderDate").innerHTML=calendarTime;
+}
+var showNext =function () {
+    var currentStart = parseInt(document.getElementById('stt').value);
+    var duration = parseInt(document.getElementById('gap').value);
+    document.getElementById("stt").value =currentStart+duration;
+    show();
+};
+var showPrevious =function () {
+    var currentStart = parseInt(document.getElementById('stt').value);
+    var duration = parseInt(document.getElementById('gap').value);
+    document.getElementById("stt").value =currentStart-duration;
+    show();
+};
+function outputUpdate(num) {
+    document.querySelector('#output').value = num;
+    document.getElementById('stt').value =num;
+    show();
+}
+function changeStep() {
+    document.getElementById("valueSlider").step =document.getElementById("gap").value;
+}
+function timeStampToCalendarDate(time) {
+
+    var date = new Date(parseInt(time));
+    var Months =["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    return date.getFullYear()+" "+Months[date.getMonth()]+" "+date.getDate()+"      "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+}
+function showChunk(start,chunk,tList,xList,yList) {
+    var data2=[];
+    var e=parseInt(start)+parseInt(chunk);
+    for(i=0;i<tList.length;i++ ){
+        if(parseInt(tList[i])<e && parseInt(tList[i])>parseInt(start)){
+            data2.push({x:xList[i] ,y:yList[i]});
+        }
+    }
+    return data2;
+}
+function doScaledTimeout2(j,start,tList,chunk,xList,yList) {
+    setTimeout(function() {
+        document.getElementById('stt').value=start;
+        document.getElementById('output').value=start;
+        setCalendarDate(start);
+
+        var data2=showChunk(start,chunk,tList,xList,yList);
+
+        draw(data2);
+    }, j * 300);
+}
